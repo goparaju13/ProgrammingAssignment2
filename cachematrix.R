8b3849c7bb5eb26a6e71bb25f41c5352c4576651
goparaju13 <goparaju248@gmail.com>
2015-03-22 14:55:33 -0700

    Added code to cacheMatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
+## makeCacheMatrix method can get or set the value of the matrix and get or set the value of the inverse of the matrix
 ## functions do
 
-## Write a short comment describing this function
+## gets or sets the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+
+        minv <- NULL
+        set <- function(y) {
+                x <<- y
+                minv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) minv <<- solve
+        getinverse <- function() minv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## Gets the inverse if available in the cache or else calculates it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+                minv <- x$getinverse()
+                if(!is.null(minv)) {
+                        message("getting cached data")
+                        return(minv)
+                }
+                data <- x$get()
+                minv <- solve(data, ...)
+                x$setinverse(minv)
+                minv
+        
 }
